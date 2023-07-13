985881e8785620c6dc4fbebbe0588e8cf4fbe825
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-13 22:51:25 +0200

    Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Card from './components/Card';
+import Card from './components/Cards';
 import CardDetails from './components/CardDetails';
 import Header from './components/Header';
 
