dd37472344f6c3bcbe6f447581bc59b5a6f7f114
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-13 22:28:48 +0200

    Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  Navbar  from 'react-bootstrap/Navbar';
 import  Container  from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import  Badge  from '@mui/material/Badge';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { NavLink } from 'react-bootstrap';
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
-        <Navbar bg="primary" data-bs-theme="light">
+        <Navbar bg="dark" data-bs-theme="dark" style={{ height: "60px" }}>
           <Container>
-            <Navbar.Brand href="#home">Add to Cart</Navbar.Brand>
+            <NavLink to="/" className="text-decoration-none text-light mx-3" href="#home">Add to Cart</NavLink>
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
+                <NavLink to="/" className="text-decoration-none text-light" href="#home">Home</NavLink>
               </Nav>
-              <Badge badgeContent={4} color="primary">
+              <Badge 
+                 id="basic-button"
+                 aria-controls={open ? 'basic-menu' : undefined}
+                 aria-haspopup="true"
+                 aria-expanded={open ? 'true' : undefined}
+                 onClick={handleClick}
+                badgeContent={4} color="primary"
+              >
                 <i className='fa-solid fa-cart-shopping text-light' style={{fontSize:25, cursor:'pointer'}}></i>
               </Badge>
           </Container>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <div className='card_details d-flex justify-content-center align-items-center' style={{width:"24rem", padding:10, position:"relative"}}> 
+              <i className='fas fa-close smallclose' onClick={handleClose} style={{position: "absolute", top:2, right:10, fontSize:23, cursor:'pointer'}}/>
+              <p>Your carts is empty</p>
+              <img src='./cart.gif' alt='' className='emptycart_img' style={{width:"5rem",padding:10}} />
+            </div>
+          </Menu>
         </Navbar>
     </>
   )
