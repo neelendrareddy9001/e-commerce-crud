89ab6151f6b7030572ee3a21f7166e0ce218e8df
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-13 23:28:55 +0200

    Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import Card from './components/Cards';
 import CardDetails from './components/CardDetails';
 import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {Routes, Route} from 'react-router-dom';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <>
       <Header/>
       <Routes>
-        <Route path='/' element={<Card/>} />
+        <Route path='/' element={<Cards/>} />
         <Route path='/cart' element={<CardDetails/>} />
       </Routes>
     </>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  Card  from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import CardsData from './CardData';
+import "./styles.css";
 
 
 const Cards = () => {
+  const [data, setData] = useState(CardsData);
+  console.log(data);
   return (
     <div className='container mt-3'>
       <h2 className='text-center'>Add to Cart Projects</h2>
-      <div className='row'>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+      <div className='row d-flex justify-content-centner align-items-center'>
+        {data.map((element, id) => {
+          return(
+            <>
+              <Card style={{ width: '18rem', border: "none" }} className='mx-2 mt-4 card_style'>
+                <Card.Img variant="top" src={element.imgdata} style={{height: "16rem"}} className='mt-3'/>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </>
+          )
+        }
+        )}
       </div>
     </div>
   )
