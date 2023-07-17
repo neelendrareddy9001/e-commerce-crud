c42a7251c523209ea78fe79766f1c1245cdfdf7d
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-17 22:42:26 +0200

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,66 +1,60 @@
-import React, { useState } from 'react';
-import  Navbar  from 'react-bootstrap/Navbar';
-import  Container  from 'react-bootstrap/Container';
+import React,{useState} from "react";
+import './styles.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import  Badge  from '@mui/material/Badge';
+import NavLink from 'react-bootstrap/NavLink';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { NavLink } from 'react-bootstrap';
-import {  useSelector } from 'react-redux';
+import  Badge from "@mui/material/Badge";
+
 
 const Header = () => {
-
-  const getData = useSelector((state) => state.cartreducer.carts);
-  console.log(getData);
-
-
-  
-
   const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
   return (
     <>
-        <Navbar bg="dark" data-bs-theme="dark" style={{ height: "60px" }}>
+        <Navbar bg="dark" variant="dark" style={{height: "60px"}}>
           <Container>
-            <NavLink to="/" className="text-decoration-none text-light mx-3" href="#home">Add to Cart</NavLink>
-              <Nav className="me-auto">
-                <NavLink to="/" className="text-decoration-none text-light" href="#home">Home</NavLink>
-              </Nav>
-              <Badge 
-                 id="basic-button"
-                 aria-controls={open ? 'basic-menu' : undefined}
-                 aria-haspopup="true"
-                 aria-expanded={open ? 'true' : undefined}
-                 onClick={handleClick}
-                badgeContent={getData.length} color="primary"
-              >
-                <i className='fa-solid fa-cart-shopping text-light' style={{fontSize:25, cursor:'pointer'}}></i>
-              </Badge>
+            <NavLink href="#home" className="text-decoration-none text-light">Add to Cart</NavLink>
+            <Nav className="me-auto">
+              <Nav.Link href="#home" className="text-decoration-none text-light">Home</Nav.Link>
+            </Nav>
+            <Badge 
+              badgeContent={4} color="primary"
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <i class="fa-solid fa-cart-shopping text-light" style={{fontSize:25, cursor:"pointer"}} />
+            </Badge>
           </Container>
           <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-          >
-            <div className='card_details d-flex justify-content-center align-items-center' style={{width:"24rem", padding:10, position:"relative"}}> 
-              <i className='fas fa-close smallclose' onClick={handleClose} style={{position: "absolute", top:2, right:10, fontSize:23, cursor:'pointer'}}/>
-              <p>Your carts is empty</p>
-              <img src='./cart.gif' alt='' className='emptycart_img' style={{width:"5rem",padding:10}} />
-            </div>
-          </Menu>
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <div className="card_details d-flex justify-content-center align-items-center" style={{width:"24rem", padding:10, position:"relative"}}>
+          <i onClick={handleClose} className="fas fa-close smallclose" style={{position:"absolute", top:2,right:20, fontSize:23, cursor:"pointer"}}></i>
+          <p>Your carts is empty</p>
+          <img src="./cart.gif" alt="" className="emptycart_img" style={{width:"5rem", padding:10}}/>
+        </div>
+      </Menu>
         </Navbar>
     </>
   )
 }
 
-export default Header
+export default Header;
